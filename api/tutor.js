311f70e2eaeda115efb6f96aa4ce0c3f08ecd332
vananhtranphan-beep <vananhtranphan@gmail.com>
2026-08-04 18:44:35 +0700

    Update tutor.js

--- api/tutor.js
+++ api/tutor.js
@@ -26,7 +26,7 @@ Nhiệm vụ của bạn:
 
     const chatCompletion = await groq.chat.completions.create({
       messages: chatHistory,
-      model: "llama-3.1-70b-versatile",
+      model: "llama3-8b-8192",
       temperature: 0.7,
     });
 
